ddce674658344ff843d77e8884bf41280c34a010
ramzi-omari <ramziomari1995@gmail.com>
2021-07-05 16:29:11 +0100

    nested Router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./App.css"
 import SignUpIn from "./components/SignUpIn"
 import Accueil from "./components/Accueil"
+import tst from "./components/tst"
+import tst2 from "./components/tst2"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        App
-        <Route path="/" component={SignUpIn} exact></Route>
-        <Route path="/accueil" component={Accueil}></Route>
-        {/* DO NOT FORGET TO CHANGE <a href> to <Link to=""> to avoid refreshing the page  */}
-      </div>
+      <Route path="/sign" component={SignUpIn} exact></Route>
+      <Route path="/" component={Accueil}>
+        <Header></Header>
+        <Route path="/tst" component={tst}></Route>
+        <Route path="/tst2" component={tst2}></Route>
+        <Footer></Footer>
+      </Route>
+      {/* DO NOT FORGET TO CHANGE <a href> to <Link to=""> to avoid refreshing the page  */}
     </Router>
   )
 }
---
+++ src/components/tst.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const tst = () => {
+  return (
+    <div>
+      <h2>tst1</h2>
+    </div>
+  )
+}
+
+export default tst

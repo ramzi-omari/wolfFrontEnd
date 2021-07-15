eee08ed7de2cb0c063a2e56ddd2341f15f2b5ec0
ramzi-omari <ramziomari1995@gmail.com>
2021-07-15 12:09:58 +0100

    combine reducer in store 'add userDetails'

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers"
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+} from "./reducers/userReducers"
 
 const reducer = combineReducers({
   // HACENE Combine all reducers here
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
